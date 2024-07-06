4cfa0824442b97e5ca28757c11c7f03cd9cb597c
Dmitriy <sirdima2014@gmail.com>
2024-07-06 20:24:26 +0300

    build:
    build:
    
    Написал docker-compose
    Service:
      -pgadmin4(проверка бд)
    
    -php-yii2(apache2)
      -postgres(присутсвует dump whishlist)
    
    db.php был
    переделан для работы с докером
    Форма регистрации работает

--- config/db.php
+++ config/db.php
@@ -2,11 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=WishList',
-
+    'dsn' => 'pgsql:host=172.20.0.2;port=5432;dbname=wishlist',
     'password' => '!mysqlpassw567!',  'username' => 'postgres',
-    'charset' => 'utf8',
-
+    'charset' => 'utf8'
     // Schema cache options (for production environment)
 //    'enableSchemaCache' => true,
 //    'schemaCacheDuration' => 60,
