e3310086475d85fae9e5cfca6ca2b3e7c1b25053
Dmitriy <sirdima2014@gmail.com>
2024-07-13 21:43:46 +0300

    fix(add userid columb to wishlist table):

---
+++ data/init2.sql
@@ -0,0 +1,603 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.19
+-- Dumped by pg_dump version 12.19
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
+--
+-- Name: add_user(text, text, text, text); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.add_user(p_name text, p_surname text, p_email text, p_password text)
+    LANGUAGE plpgsql
+    AS $$BEGIN
+    -- Проверяем, есть ли пользователь с таким email
+    IF NOT EXISTS (SELECT 1 FROM users WHERE email = p_email) THEN
+        BEGIN
+            INSERT INTO users (name, surname, email, password, registration_date)
+            VALUES (
+                p_name,
+                p_surname,
+                p_email,
+                p_password,
+                CURRENT_TIMESTAMP
+            );
+            RAISE NOTICE 'Пользователь успешно добавлен';
+        EXCEPTION
+            WHEN OTHERS THEN
+                RAISE NOTICE 'Ошибка при добавлении пользователя';
+        END;
+    ELSE
+        RAISE NOTICE 'Пользователь с таким email уже существует';
+    END IF;
+END;$$;
+
+
+ALTER PROCEDURE public.add_user(p_name text, p_surname text, p_email text, p_password text) OWNER TO postgres;
+
+--
+-- Name: add_wish(integer, text, text, text, integer, text, text); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.add_wish(p_userid integer, p_name text, p_surname text, p_category text, p_price integer, p_img_path text, p_url text)
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    INSERT INTO wishlist(
+        userid,
+        name,
+        surname,
+        category,
+        price,
+        img_path,
+        url
+    )
+    VALUES (
+        p_userid,
+        p_name,
+        p_surname,
+        p_category,
+        p_price,
+        p_img_path,
+        p_url
+    );
+END;
+$$;
+
+
+ALTER PROCEDURE public.add_wish(p_userid integer, p_name text, p_surname text, p_category text, p_price integer, p_img_path text, p_url text) OWNER TO postgres;
+
+--
+-- Name: get_wishlist(integer); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.get_wishlist(p_userid integer) RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    user_records JSONB;
+BEGIN
+    SELECT jsonb_agg(
+        jsonb_build_object(
+            'wishid', w.wishid,
+            'name', w.name,
+            'category', w.category,
+			'price', w.price,
+			'img_path', w.img_path,
+			'url', w.url
+        )
+    ) INTO user_records
+    FROM wishlist w
+    WHERE w.userid = p_userid;
+
+    RETURN COALESCE(user_records, '[]'::JSONB);  -- возвращаем пустой массив JSON, если нет записей
+END;
+$$;
+
+
+ALTER FUNCTION public.get_wishlist(p_userid integer) OWNER TO postgres;
+
+--
+-- Name: share_whish(); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.share_whish()
+    LANGUAGE sql
+    AS $_$
+CREATE OR REPLACE PROCEDURE share_wish(
+    p_userid INTEGER,
+	p_friendid INTEGER,
+	p_whishid INTEGER,
+    p_master_name TEXT,
+    p_friend_name TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO share_whish (
+		userid,
+		friendid,
+		whishid,
+    	master_name,
+    	friend_name,
+	)
+	VALUES (
+		p_userid,
+		p_friendid,
+		p_whishid,
+    	p_master_name,
+    	p_friend_name,
+	);
+END;
+$$;
+$_$;
+
+
+ALTER PROCEDURE public.share_whish() OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: wish; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.wish (
+    wishid integer NOT NULL,
+    userid integer NOT NULL,
+    name text NOT NULL,
+    price integer,
+    img_path text,
+    url text
+);
+
+
+ALTER TABLE public.wish OWNER TO postgres;
+
+--
+-- Name: WhishList_whishID_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public."WhishList_whishID_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public."WhishList_whishID_seq" OWNER TO postgres;
+
+--
+-- Name: WhishList_whishID_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public."WhishList_whishID_seq" OWNED BY public.wish.wishid;
+
+
+--
+-- Name: category; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.category (
+    categoryid integer NOT NULL,
+    userid integer NOT NULL,
+    name text NOT NULL
+);
+
+
+ALTER TABLE public.category OWNER TO postgres;
+
+--
+-- Name: category_category_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.category_category_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.category_category_id_seq OWNER TO postgres;
+
+--
+-- Name: category_category_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.category_category_id_seq OWNED BY public.category.categoryid;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    userid integer NOT NULL,
+    name text NOT NULL,
+    surname text,
+    email text NOT NULL,
+    password text NOT NULL,
+    registration_date date NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: wish_wishlist; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.wish_wishlist (
+    uniqueid integer NOT NULL,
+    wishid integer NOT NULL,
+    wishlistid integer NOT NULL
+);
+
+
+ALTER TABLE public.wish_wishlist OWNER TO postgres;
+
+--
+-- Name: wish_wishlist_uniqueid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.wish_wishlist_uniqueid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.wish_wishlist_uniqueid_seq OWNER TO postgres;
+
+--
+-- Name: wish_wishlist_uniqueid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.wish_wishlist_uniqueid_seq OWNED BY public.wish_wishlist.uniqueid;
+
+
+--
+-- Name: wishlist; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.wishlist (
+    wishlistid integer NOT NULL,
+    share_url text,
+    "isPrivate" boolean NOT NULL,
+    userid integer NOT NULL
+);
+
+
+ALTER TABLE public.wishlist OWNER TO postgres;
+
+--
+-- Name: wishlist_catgegory; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.wishlist_catgegory (
+    uniqueid integer NOT NULL,
+    wishlistid integer NOT NULL,
+    categoryid integer NOT NULL
+);
+
+
+ALTER TABLE public.wishlist_catgegory OWNER TO postgres;
+
+--
+-- Name: wishlist_catgegory_uniqueid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.wishlist_catgegory_uniqueid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.wishlist_catgegory_uniqueid_seq OWNER TO postgres;
+
+--
+-- Name: wishlist_catgegory_uniqueid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.wishlist_catgegory_uniqueid_seq OWNED BY public.wishlist_catgegory.uniqueid;
+
+
+--
+-- Name: wishlist_wishid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.wishlist_wishid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.wishlist_wishid_seq OWNER TO postgres;
+
+--
+-- Name: wishlist_wishid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.wishlist_wishid_seq OWNED BY public.wishlist.wishlistid;
+
+
+--
+-- Name: category categoryid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category ALTER COLUMN categoryid SET DEFAULT nextval('public.category_category_id_seq'::regclass);
+
+
+--
+-- Name: wish wishid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wish ALTER COLUMN wishid SET DEFAULT nextval('public."WhishList_whishID_seq"'::regclass);
+
+
+--
+-- Name: wish_wishlist uniqueid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wish_wishlist ALTER COLUMN uniqueid SET DEFAULT nextval('public.wish_wishlist_uniqueid_seq'::regclass);
+
+
+--
+-- Name: wishlist wishlistid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wishlist ALTER COLUMN wishlistid SET DEFAULT nextval('public.wishlist_wishid_seq'::regclass);
+
+
+--
+-- Name: wishlist_catgegory uniqueid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wishlist_catgegory ALTER COLUMN uniqueid SET DEFAULT nextval('public.wishlist_catgegory_uniqueid_seq'::regclass);
+
+
+--
+-- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.category (categoryid, userid, name) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (userid, name, surname, email, password, registration_date) FROM stdin;
+5	Дмитрий	1	1	$1$iaLEqEBm$GLkZN.rGM9x8I3SlQOT.30	2024-07-03
+6	505	51	521	$1$wowoAoP3$7bLgHnjjMZTBVrHY0auAg.	2024-07-03
+7	505	51	521	$1$InurLI9B$HpAXFhEoV6efHBpql.h380	2024-07-03
+8	Дмитрий	\N	d12m@mail.ru	$1$em5aMPuw$I2U4/c89hVnyfghcxJH/Q.	2024-07-05
+9	Foma	\N	foma@mail.ru	$1$xUnQEjSi$DtAbcba2HGjBp0b.LUy6I.	2024-07-05
+10	Дмитрий	\N	senior1@yandex.ru	$1$nrUwwtT1$eEekjj0BLyYD8iXJQN43p0	2024-07-05
+11	32132151	\N	senior1@yandex.ru	$1$sOtg26WW$ETyftoHPE4iBOPDAymX.K0	2024-07-05
+12	321	\N	seni421or1@yandex.ru	$1$a12PBtNe$lWAXe5EFSEsTHYGZzUtIq1	2024-07-05
+13	Дмитрий	1	senior1@yandex.ru	$1$IY6QrbwB$prqHgCv1XyDcfzsxjsUwv/	2024-07-05
+14	Дмитрий	1	senior1@yandex.ru	$1$4vOMYHTa$d2z6wb74nxxLH6FRhlkFb0	2024-07-05
+15	Дмитрий	\N	senior12@yandex.ru	$1$1B0CNM1y$AUP66pXabKaK14/jVBlpI1	2024-07-05
+16	Дмитрий	\N	senio321r12@yandex.ru	$1$n3CuOpSb$uPjHh4SKZJSV0ouIjh2.S0	2024-07-05
+\.
+
+
+--
+-- Data for Name: wish; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.wish (wishid, userid, name, price, img_path, url) FROM stdin;
+3	5	Дмитрий	30000	/img_db	'https://market.yandex.ru/product--skx007j1/652474393?hid=91259&show-uid=17200209995801947551616003&from=search&text=seiko%20skx007&rs=eJxVjTFOwzAYhe2SIhOB5DEqA1anbrGdpInXLkhMFRewnB9LMQ1JVKcCdWKF03AFpApxDZjYQKwsRGVi-aT39J4-WU7Pw2Nv3aplfnXHeU5f3t8OpgFBdLwn
+4	5	Дмитрий	6400	/img_db	https://market.yandex.ru/product--muzhskie-amfibiia-720073/1823360993?hid=91259&show-uid=17200223171919622451316005&from=search&text=амфибия&rs=eJwlzjFLAzEUwPGk9mw8UG4sdTB0ytYkbe_o4uDoJH4AwzW90JPaHr0WoZOCKBXBz-Diat06uCgiOhSuuDmpk6CodJK6mNTlP7z8Hi_8AuZX7aV
+6	5	1	1	1	1
+\.
+
+
+--
+-- Data for Name: wish_wishlist; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.wish_wishlist (uniqueid, wishid, wishlistid) FROM stdin;
+\.
+
+
+--
+-- Data for Name: wishlist; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.wishlist (wishlistid, share_url, "isPrivate", userid) FROM stdin;
+\.
+
+
+--
+-- Data for Name: wishlist_catgegory; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.wishlist_catgegory (uniqueid, wishlistid, categoryid) FROM stdin;
+\.
+
+
+--
+-- Name: WhishList_whishID_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."WhishList_whishID_seq"', 6, true);
+
+
+--
+-- Name: category_category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.category_category_id_seq', 1, false);
+
+
+--
+-- Name: wish_wishlist_uniqueid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.wish_wishlist_uniqueid_seq', 1, false);
+
+
+--
+-- Name: wishlist_catgegory_uniqueid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.wishlist_catgegory_uniqueid_seq', 1, false);
+
+
+--
+-- Name: wishlist_wishid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.wishlist_wishid_seq', 1, false);
+
+
+--
+-- Name: wish WhishList_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wish
+    ADD CONSTRAINT "WhishList_pkey" PRIMARY KEY (wishid);
+
+
+--
+-- Name: category category_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category
+    ADD CONSTRAINT category_pkey PRIMARY KEY (categoryid);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (userid);
+
+
+--
+-- Name: wish_wishlist wish_wishlist_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wish_wishlist
+    ADD CONSTRAINT wish_wishlist_pkey PRIMARY KEY (uniqueid);
+
+
+--
+-- Name: wishlist_catgegory wishlist_catgegory_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wishlist_catgegory
+    ADD CONSTRAINT wishlist_catgegory_pkey PRIMARY KEY (uniqueid);
+
+
+--
+-- Name: wishlist wishlist_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wishlist
+    ADD CONSTRAINT wishlist_pkey PRIMARY KEY (wishlistid);
+
+
+--
+-- Name: category category_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.category
+    ADD CONSTRAINT category_user_id_fkey FOREIGN KEY (userid) REFERENCES public.users(userid);
+
+
+--
+-- Name: wish userid; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wish
+    ADD CONSTRAINT userid FOREIGN KEY (userid) REFERENCES public.users(userid);
+
+
+--
+-- Name: wish_wishlist wish_wishlist_wishid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wish_wishlist
+    ADD CONSTRAINT wish_wishlist_wishid_fkey FOREIGN KEY (wishid) REFERENCES public.wish(wishid) NOT VALID;
+
+
+--
+-- Name: wish_wishlist wish_wishlist_wishlistid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wish_wishlist
+    ADD CONSTRAINT wish_wishlist_wishlistid_fkey FOREIGN KEY (wishlistid) REFERENCES public.wishlist(wishlistid) NOT VALID;
+
+
+--
+-- Name: wishlist_catgegory wishlist_catgegory_categoryid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wishlist_catgegory
+    ADD CONSTRAINT wishlist_catgegory_categoryid_fkey FOREIGN KEY (categoryid) REFERENCES public.category(categoryid) NOT VALID;
+
+
+--
+-- Name: wishlist_catgegory wishlist_catgegory_wishlistid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wishlist_catgegory
+    ADD CONSTRAINT wishlist_catgegory_wishlistid_fkey FOREIGN KEY (wishlistid) REFERENCES public.wishlist(wishlistid);
+
+
+--
+-- Name: wishlist wishlist_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.wishlist
+    ADD CONSTRAINT wishlist_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) NOT VALID;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
